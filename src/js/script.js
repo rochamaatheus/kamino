17ab2d0fbde8e81a7c1a84c6ebd0b78a298c09d2
rochamaatheus <matheussilveirarocha.sc@gmail.com>
2025-02-16 19:02:08 -0300

    Atualização de estrutura

--- src/js/script.js
+++ src/js/script.js
@@ -2,10 +2,7 @@ import { inicializarMenuMobile } from './modulos/menuMobile.js';
 import { inicializarDropdown } from './modulos/dropdown.js';
 import { inicializarAnimacoesScroll } from './modulos/animacoesScroll.js';
 import { setupScrollAnimation } from './modulos/scrollAnimation.js';
-import { initTimeline } from './modulos/timeline.js';
 import { inicializarFormulario } from './modulos/formulario.js';
-
-/* Importa as funções do marquee */
 import { initMarqueeAllNichos } from './modulos/marquee.js';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -20,18 +17,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // 4) IntersectionObserver (ScrollAnimation)
   setupScrollAnimation('.cards-servicos-nicho');
-  setupScrollAnimation('#comparativo', false);
-  setupScrollAnimation('#metodologia', false);
 
-  // 5) Timeline
-  initTimeline();
-
-  // 6) Formulário
+  // 5) Formulário
   inicializarFormulario();
 
-  // 7) Ajustes adicionais (exemplo)
-  document.documentElement.style.setProperty('--circle-offset', '25px');
-
-  // 8) Marquee e Botões de Nichos (com velocidade=1, por ex.)
+  // 6) Marquee e Botões de Nichos (com velocidade=1, por ex.)
   initMarqueeAllNichos(1, 48);
 });
