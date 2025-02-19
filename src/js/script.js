cdd1bbc862c60b5180f5a37f3bb21048efc2e1fc
rochamaatheus <matheussilveirarocha.sc@gmail.com>
2025-02-19 17:33:05 -0300

    melhorias gerais

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,9 @@
 import { inicializarMenuMobile } from './modulos/menuMobile.js';
-import { inicializarDropdown } from './modulos/dropdown.js';
 import { inicializarAnimacoesScroll } from './modulos/animacoesScroll.js';
 import { setupScrollAnimation } from './modulos/scrollAnimation.js';
 import { inicializarFormulario } from './modulos/formulario.js';
 import { initMarqueeAllNichos } from './modulos/marquee.js';
+import { TestemunhosCarousel } from './modulos/testemunhoCarousel.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('a[href^="#"]').forEach(link => {
@@ -39,18 +39,18 @@ document.addEventListener('DOMContentLoaded', () => {
   // 1) Menu Mobile
   inicializarMenuMobile();
 
-  // 2) Dropdown (hover)
-  inicializarDropdown();
-
-  // 3) Animações gerais de scroll
+  // 2) Animações gerais de scroll
   inicializarAnimacoesScroll();
 
-  // 4) IntersectionObserver (ScrollAnimation)
+  // 3) IntersectionObserver (ScrollAnimation)
   setupScrollAnimation('.cards-servicos-nicho');
 
-  // 5) Formulário
+  // 4) Formulário
   inicializarFormulario();
 
-  // 6) Marquee e Botões de Nichos (com velocidade=1, por ex.)
+  // 5) Marquee e Botões de Nichos (com velocidade=1, por ex.)
   initMarqueeAllNichos(1, 48);
+
+  // 6) Carousel testemunho
+  const carousel = new TestemunhosCarousel('#testemunhos');
 });
