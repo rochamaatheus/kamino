0635d9571cdbb71fe13d5537a0bf108a6761e3af
rochamaatheus <matheussilveirarocha.sc@gmail.com>
2025-02-20 17:57:24 -0300

    Melhorias gerais

--- src/js/modulos/formulario.js
+++ src/js/modulos/formulario.js
@@ -1,44 +1,4 @@
-export function inicializarFormulario() {
-  const form = document.getElementById('contactForm');
-  if (!form) return;
-
-  const formError = document.getElementById('formError');
-  const camposObrigatorios = [
-    'nome',
-    'email',
-    'telefone',
-    'base_investimento',
-    'base_funcionarios',
-    'mensagem',
-  ];
-
-  form.addEventListener('submit', e => {
-    e.preventDefault();
-
-    let camposVazios = false;
-
-    // Verificar campos obrigatórios
-    camposObrigatorios.forEach(campoId => {
-      const campo = document.getElementById(campoId);
-      if (campo && campo.value.trim() === '') {
-        camposVazios = true;
-        campo.classList.add('error');
-      } else {
-        campo?.classList.remove('error');
-      }
-    });
-
-    if (camposVazios) {
-      formError.style.display = 'block';
-      return;
-    }
-
-    formError.style.display = 'none';
-    alert(
-      `Olá, ${
+export function inicializarFormulario(){const form=document.getElementById('contactForm');if(!form)return;const formError=document.getElementById('formError');const camposObrigatorios=['nome','email','telefone','base_investimento','base_funcionarios','mensagem',];form.addEventListener('submit',e=>{e.preventDefault();let camposVazios=!1;camposObrigatorios.forEach(campoId=>{const campo=document.getElementById(campoId);if(campo&&campo.value.trim()===''){camposVazios=!0;campo.classList.add('error')}else{campo?.classList.remove('error')}});if(camposVazios){formError.style.display='block';return}
+formError.style.display='none';alert(`Olá, ${
         document.getElementById('nome').value
-      }! Recebemos sua mensagem e em breve retornaremos.`,
-    );
-    form.reset();
-  });
-}
+      }! Recebemos sua mensagem e em breve retornaremos.`,);form.reset()})}
